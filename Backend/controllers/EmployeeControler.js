b9700b91b6ba6e76024b5357fc4512d31edb39a3
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-02-20 04:21:22 +0000

    code added but not posting data

---
+++ Backend/controllers/EmployeeControler.js
@@ -0,0 +1,21 @@
+const Employee = require("../models/Employee");
+
+const createEmployee = async (req, res) => {
+  try {
+    const {name, email, phone, city} = req.body;
+
+    const employee = new Employee({
+        name,
+        email,
+        phone,
+        city
+    })
+    await employee.save()
+    res.status(201).json(employee)
+  } catch (error) {
+     console.log("server error", error)
+     res.status(500).json({message: 'server error'})
+  }
+}
+
+module.exports = {createEmployee};
--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const employeeRoutes = require('./routes/EmployeeRouter');
 
 const app = express();
+require('dotenv').config();
+app.use(bodyParser.json())
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("database connected")
+}).catch((err) => {
+    console.log(err)
+})
+
+
 
 app.get("/", (req, res) => {
     res.send("hello world")
 })
+
+app.use("/employees", employeeRoutes);
 
 const PORT = process.env.PORT || 5002;
 
