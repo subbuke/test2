3d4eb4764474cf5f355f75ab5ad0cef4c86280b4
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-02-24 11:44:02 +0000

    all removed

--- Backend/routes/EmployeeRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const employeeController = require("../controllers/EmployeeControler");
-const Employee = require("../models/Employee");
-
-router.post('/add-emp', employeeController.createEmployee);
-
-module.exports = router;
