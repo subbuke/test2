9c4c029bd43b0e1e40eb1203e3a96cb6629b8c7b
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-02-26 10:22:52 +0000

    upadte should add

--- Backend/index.js
+++ Backend/index.js
@@ -19,10 +19,41 @@ app.get('/', (req, res) => {
     res.send("server running sucessfully")
 });
 
+//GET request
 app.get("/students", (req, res) => {
     student.find()
     .then(student => res.json(student))
     .catch(err => console.log(err))
+})
+
+//post request
+app.post("/post", (req, res) => {
+    const {name, group} = req.body;
+
+    //create new user 
+    const newStudent = new student({
+      name:name,
+      group: group
+
+    })
+
+    //save the new user to database
+    newStudent.save()
+    .then(() => res.status(201).json({message: "user added sucessfully"}))
+    .catch( err => res.status(400).json("fetching error" + err))
+})
+
+//Delete request
+app.delete("/delete", (req, res) => {
+    const {id} = req.params;
+    student.findByIdAndDelete("67bd430fb10f5748bac81c04")
+    .then(() => res.status(201).json({message: "user deleted successfully"}))
+    .catch(err => res.status(400).json("error while deleting" + err))
+})
+
+//update method
+app.put("/update", (req, res) => {
+    
 })
 
 const PORT = process.env.PORT || 8000;
